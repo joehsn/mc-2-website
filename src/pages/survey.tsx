b726e423056773d83e31b6d481c94c5b1926bce9
Youssef Hassan <89865074+joehsn@users.noreply.github.com>
2023-03-13 15:14:56 +0200

    read to preview

--- src/pages/survey.tsx
+++ src/pages/survey.tsx
@@ -2,6 +2,14 @@ import dynamic from "next/dynamic";
 import Head from "next/head";
 import React from "react";
 
+import { Tajawal } from "next/font/google";
+import Link from "next/link";
+
+const tajawal = Tajawal({
+  weight: ["200", "300", "400", "500", "700", "800", "900"],
+  subsets: ["arabic", "latin"],
+});
+
 const SurveyComponent = dynamic(() => import("@/components/survey"), {
   ssr: false,
 });
@@ -14,12 +22,16 @@ function Survey({}: Props) {
       <Head>
         <title>اسنبيان</title>
       </Head>
-      <div>
-        <nav className="w-full py-8 text-center">NAVBAR</nav>
-        <div className="mx-auto w-[550px]">
-          <SurveyComponent />
+      <main className={tajawal.className}>
+        <nav className="w-full py-8 text-center">
+          <Link href="/">الصفحة الرئيسية</Link>
+        </nav>
+        <div className="w-full bg-third py-32">
+          <div className="mx-auto w-[768px]">
+            <SurveyComponent />
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: "class",
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#c84656",
+        second: "#575657",
+        third: "#ececee",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-flip")],
 };
